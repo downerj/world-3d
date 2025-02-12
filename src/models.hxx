e340726b9303f011567ecb548ade7cc7b8e54b75
Jimmy Downer <jamesdanieldowner@gmail.com>
2025-02-11 18:21:35 -0700

    BasicTriangle model geometry

--- src/models.hxx
+++ src/models.hxx
@@ -1,4 +1,25 @@
 #ifndef MODELS_HXX
 #define MODELS_HXX
 
+class Geometry {
+public:
+  virtual auto getVertices() const -> const float* = 0;
+  virtual auto getColors() const -> const float* = 0;
+  virtual auto getIndices() const -> const unsigned short* = 0;
+  virtual auto getVertexCount() const -> int = 0;
+  virtual auto getColorCount() const -> int = 0;
+  virtual auto getIndexCount() const -> int = 0;
+  virtual ~Geometry() noexcept = 0;
+};
+
+class BasicTriangle : public Geometry {
+public:
+  auto getVertices() const -> const float* final;
+  auto getColors() const -> const float* final;
+  auto getIndices() const -> const unsigned short* final;
+  auto getVertexCount() const -> int final;
+  auto getColorCount() const -> int final;
+  auto getIndexCount() const -> int final;
+};
+
 #endif // MODELS_HXX
